7ec6bc8585245b4ebacc340cea6a4041e2e7fa0c
lanzi97 <lanzi97@hotmail.it>
2017-03-08 12:18:49 +0100

    Added TablesActivity, added toolbar_back icon, changed icons color Changed activity theme managment

--- app/src/main/java/com/laprogramming/restaurantorders/LoginActivity.java
+++ app/src/main/java/com/laprogramming/restaurantorders/LoginActivity.java
@@ -1,8 +1,13 @@
 package com.laprogramming.restaurantorders;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,6 +15,21 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Equivalent
+        //EditText userEditText=(EditText) findViewById(R.id.login_user);
+        //String username=userEditText.getText().toString();
+        final String username=((EditText) findViewById(R.id.login_user)).getText().toString();
+        final String password=((EditText) findViewById(R.id.login_password)).getText().toString();
+
+        //Handle floatingActionButton click
+        FloatingActionButton floatButton=(FloatingActionButton) findViewById(R.id.login_float_button);
+        floatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userCheck(username,password);
+            }
+        });
     }
 
     /*
@@ -18,9 +38,11 @@ public class LoginActivity extends AppCompatActivity {
     public void userCheck(String username, String password){
         boolean access=true;    //TODO remove true
 
+        Toast.makeText(this, "Username = "+ username + "\nPassword = "+ password, Toast.LENGTH_LONG).show();
+
         if(access == true){
-            //Intent verified = new Intent(this, HomeActivity.class);   //TODO CREATE HOME ACTIVITY
-            //startActivity(verified);
+            Intent verified = new Intent(this, TablesActivity.class);
+            startActivity(verified);
         }
     }
 }
--- app/src/main/java/com/laprogramming/restaurantorders/TablesActivity.java
+++ app/src/main/java/com/laprogramming/restaurantorders/TablesActivity.java
@@ -1,7 +1,11 @@
 package com.laprogramming.restaurantorders;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 public class TablesActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class TablesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tables);
+        initToolbar();
+    }
+
+    private void initToolbar(){
+        //TODO put string constant in /res/strings.xml file
+        Toolbar toolbar=(Toolbar) findViewById(R.id.tables_toolbar);
+        toolbar.setTitle("Tables");
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(R.drawable.ic_toolbar_arrow_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Pressed back!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
